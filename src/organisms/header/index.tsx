55f89638975dbc93e55cdaeaaee529016edee714
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-04-01 15:56:38 +0300

    refactor: add props to header

--- src/organisms/header/index.tsx
+++ src/organisms/header/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import Navigation from '../../molecules/navigation-menu';
+import Navigation, {
+  navigationLinkTypes,
+} from '../../molecules/navigation-menu';
 import AvatarWrapper from '../../molecules/avatar';
 import { UserNav, userNavTypes } from '../profile-menu/index';
 
 interface DashboardHeaderProps {
   children?: React.ReactNode;
   logo?: string;
+  navMenu: navigationLinkTypes[];
   title?: string;
   userNav?: userNavTypes;
 }
@@ -16,11 +19,12 @@ export default function DashboardHeader({
   title,
   logo,
   userNav,
+  navMenu,
 }: DashboardHeaderProps) {
   return (
     <div className="flex items-center justify-between px-2 w-100">
       <AvatarWrapper text="UR" url={logo} sideText={title} />
-      <Navigation />
+      <Navigation navigationLinks={navMenu} />
       {children && <div className="flex items-center gap-2">{children}</div>}
       <UserNav {...userNav} />
     </div>
