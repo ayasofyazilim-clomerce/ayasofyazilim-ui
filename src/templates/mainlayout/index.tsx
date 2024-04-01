858d053c8db5861511b9b71d342a7d3a3e2bb042
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-04-01 16:17:15 +0300

    refactor: pass data to mainlayout

--- src/templates/mainlayout/index.tsx
+++ src/templates/mainlayout/index.tsx
@@ -2,11 +2,13 @@ import React, { JSXElementConstructor, ReactElement } from 'react';
 import { userNavTypes } from 'src/organisms/profile-menu';
 import DashboardHeader from '../../organisms/header';
 import Sidebar, { MenuProps } from '../../molecules/side-bar/index';
+import { navigationLinkTypes } from '../../molecules/navigation-menu';
 
 export type mainLayoutProps = {
-  children: ReactElement<any, string | JSXElementConstructor<any>>;
+  children?: ReactElement<any, string | JSXElementConstructor<any>>;
   logo?: string;
   menus?: MenuProps[];
+  navMenu: navigationLinkTypes[];
   title?: string;
   userNav?: userNavTypes;
 };
@@ -20,11 +22,17 @@ export default function Mainlayout({
   children,
   menus,
   userNav,
+  navMenu,
 }: mainLayoutProps) {
   // porps: DashboardProps
   return (
     <>
-      <DashboardHeader logo={logo} title={title} userNav={userNav} />
+      <DashboardHeader
+        logo={logo}
+        title={title}
+        userNav={userNav}
+        navMenu={navMenu}
+      />
       <div className="flex">
         <Sidebar menus={menus} />
         <div className="flex-col w-full h-full">{children}</div>
