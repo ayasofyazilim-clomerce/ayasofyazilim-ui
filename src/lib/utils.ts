0c2446199fd9b1195fe7967b8ea9c3266c6c117d
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-10-31 15:39:15 +0300

    chore:export cva

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx"
 import { JSX } from "react";
 import { twMerge } from "tailwind-merge"
 
+export * from "class-variance-authority"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
